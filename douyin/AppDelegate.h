a0c08079ad69598eab5a33fd66e4cc3232f4c858
konglee <konglee873@gmail.com>
2018-03-20 12:55:56 +0800

    Initial Commit

---
+++ douyin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  douyin
+//
+//  Created by kong on 2018/3/20.
+//  Copyright © 2018年 kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
