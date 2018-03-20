524a5a04d3ec5b976877adf0d2f196412b953be8
konglee <konglee873@gmail.com>
2018-03-20 15:32:02 +0800

    cocoa pods

--- douyin/ViewController.m
+++ douyin/ViewController.m
@@ -17,12 +17,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int res = getAllWays2(10);
+    int res1 = getAllWays(10);
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+int getAllWays(int n)
+{
+    if (n <= 2)
+    {
+        return n;
+    }
+    if (n == 3)
+    {
+        return 4;
+    }
+    int a = 1;
+    int b = 2;
+    int c = 4;
+    int temp = 0;
+    for (int i = 4; i <= n; i++)
+    {
+        temp = a + b + c;
+        a = b;
+        b = c;
+        c = temp;
+    }
+    return temp;
+}
+
+int getAllWays2(int n)
+{
+    if (n <= 2)
+    {
+        return n;
+    }
+    if (n == 3)
+    {
+        return 4;
+    }
+    return getAllWays2(n-1) + getAllWays2(n-2) + getAllWays2(n-3);
 }
 
 
